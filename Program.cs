27ed12437dd948fbe83617df0aab7b4b0952ef3b
theshopp <124616843+theshopp@users.noreply.github.com>
2023-10-15 21:13:03 +0000

    pooop

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int number1 = 5;
+            Console.WriteLine(number1);
+            int number2 =Convert.ToInt32(Console.ReadLine());
+             int number3 = 6;
+            Console.WriteLine(number3);
+            int number4 =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("hello");
+            int num5 = 7;
+            int number6 = 8
+            int finla
+            int num5 + number6 =
+           
+            {
+                
+            }
+            
+}
+
+    
+    }
+}
