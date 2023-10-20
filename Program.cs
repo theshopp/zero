2b055bb1f498b77935eff3ba5fa3c8e3dcc9b91b
theshopp <124616843+theshopp@users.noreply.github.com>
2023-10-20 21:49:29 +0000

    gwad

--- Program.cs
+++ Program.cs
@@ -5,23 +5,17 @@ namespace MyApp // Note: actual namespace depends on the project name.
     internal class Program
     {
         static void Main(string[] args)
-        {
-           
-            int number1 = 5;
-            Console.WriteLine(number1);
-            int number2 =Convert.ToInt32(Console.ReadLine());
-             int number3 = 6;
-            Console.WriteLine(number3);
-            int number4 =Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("hello");
-            int num5 = 7;
-            int number6 = 8
-            int finla
-            int num5 + number6 =
-           
-            {
-                
-            }
+        {//calculator
+        Console.WriteLine("press any key to begin");
+            Console.ReadKey(); 
+           Console.WriteLine("input number plz");
+           int num1 =Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("input a second num plz");
+             int num2 =Convert.ToInt32( Console.ReadLine());
+             int fin = num1 + num2;
+             Console.WriteLine(fin);
+            //game
+           if ()
             
 }
 
